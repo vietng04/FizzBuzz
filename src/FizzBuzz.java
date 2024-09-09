0d377ac8cf35d1a0cf31490cba7965566c1a1e99
nguyenviet <vnguyrnhoang004@gmail.com>
2024-09-09 12:33:53 -0400

    task 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -34,24 +34,29 @@
 //}
 
 class FizzBuzz {
-    public static void main(String[] args) {
+    public static void main() {
         int i = 0;
         while (i < 100) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 && divisibleBy5) {
-                System.out.println("Fizz Buzz");
-            }
-            else if (divisibleBy3) {
-                System.out.println("Fizz");
-            }
-            else if (divisibleBy5) {
-                System.out.println("Buzz");
-            }
-            else {
-                System.out.println(i);
-            }
-            i++;
+            i = doFizzBuzz(i);
         }
+    }
+
+    public static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        }
+        else if (divisibleBy3) {
+            System.out.println("Fizz");
+        }
+        else if (divisibleBy5) {
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
     }
 }
